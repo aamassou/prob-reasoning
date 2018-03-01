8da8fae20a148ca232753bec3875da11b088c92a
Azmin Massoumi <azminmassoumi@Azmins-MacBook-Air.local>
2018-03-01 17:57:19 +0100

    add nothing observation matrix

--- robot_move.py
+++ robot_move.py
@@ -63,38 +63,74 @@ def new_move(robot):
 def get_transition_matrix():
     transitions = [[0 for a in range((grid_size**2)*4)] for b in range((grid_size**2)*4)]
     matrix_size = len(transitions)
+    #print matrix_size
 
-    for i in range(matrix_size):
-        for j in range(matrix_size):
-            index_i = i/grid_size-1
-            index_j = j/grid_size-1
+    for i in xrange(matrix_size):
+        for j in xrange(matrix_size):
+            index_i = i/4
+            index_j = j/4
             if index_i != index_j:
                 ix = index_i / grid_size
                 jx = index_j / grid_size
                 iy = index_i % grid_size
                 jy = index_j % grid_size
-                ih = i % grid_size-1
-                jh = j % grid_size-1
+                ih = i % 4
+                jh = j % 4
 
-                if abs(ix-jx) + abs(iy-jy) != 1:
-                    continue
+                if abs(ix-jx) + abs(iy-jy) == 1:
+                    #continue
+                    if ih == jh:
+                        if ((ih == WEST and (iy-jy == 1)) or (ih == SOUTH and (jx-ix == 1))
+                            or (ih == NORTH and (ix-jx == 1)) or (ih == EAST and (jy-iy == 1))):
+                            transitions[i][j] = 0.7
 
-                if ih == jh:
-                    if ((ih == WEST and (iy-jy == 1)) or (ih == SOUTH and (jx-ix == 1))
-                        or (ih == NORTH and (ix-jx == 1)) or (ih == EAST and (jy-iy == 1))):
-                        transitions[i][j] = 0.7
-
-                elif ((jh == WEST and (iy-jy == 1)) or (jh == SOUTH and (jx-ix == 1))
-                    or (jh == NORTH and (ix-jx == 1)) or (jh == EAST and (jy-iy == 1))):
-                    moves = get_valid_moves(ix, iy, ih)
-                    transitions[i][j] = 0.3/len(moves)
+                    elif ((jh == WEST and (iy-jy == 1)) or (jh == SOUTH and (jx-ix == 1))
+                        or (jh == NORTH and (ix-jx == 1)) or (jh == EAST and (jy-iy == 1))):
+                        moves = get_valid_moves(ix, iy, ih)
+                        transitions[i][j] = 0.3/float(len(moves))
     return transitions
+
+def get_ls(x, y):
+    ls = [(x-1,y-1), (x-1, y), (x-1, y+1), (x, y-1), (x, y+1), (x+1, y-1), (x+1, y), (x+1, y+1)]
+
+    for adj_x, adj_y in ls:
+        if adj_x >= grid_size or adj_x < 0 or adj_y >= grid_size or adj_y < 0:
+                ls.remove((adj_x, adj_y))
+
+    return ls
+
+def get_ls2(x, y):
+    ls2 = [(x-2, y-2), (x-2, y-1), (x-2, y), (x-2, y+1), (x-2, y+2), (x-1, y-2),
+           (x-1, y+2), (x, y-2), (x, y+2), (x+1, y-2), (x+1, y+2), (x+2, y-2),
+           (x+2, y-1), (x+2, y), (x+2, y+1), (x+2, y+2)]
+
+    for adj_x, adj_y in ls2:
+        if adj_x >= grid_size or adj_x < 0 or adj_y >= grid_size or adj_y < 0:
+                ls2.remove((adj_x, adj_y))
+
+    return ls2
+
+def get_nothing_vector():
+    n_mat = [0 for a in range((grid_size**2)*4)]
+    matrix_size = len(n_mat)
+
+    for i in xrange(matrix_size):
+        x = i/(grid_size*4)
+        y = (i/4)%grid_size
+        n_ls = 8 - len(get_ls(x,y))
+        n_ls2 = 16 - len(get_ls2(x,y))
+
+        n_mat[i] = 1.0 - 0.1 - (n_ls*0.05) - (n_ls2*0.025)
+
+    return n_mat
+
 
 class Grid:
     def __init__(self, robot):
         self.robot = robot
-        self.tran_matrix = get_transition_matrix
+        self.tran_matrix = get_transition_matrix()
         self.obs_matrices = None
+        self.nothing_vector = get_nothing_vector()
 
 class Robot(object):
     def __init__(self):
@@ -113,6 +149,8 @@ robot.x, robot.y, robot.h = x0, y0, h0
 #     wait = raw_input("PRESS ENTER TO MAKE NEXT MOVE")
 #     get_next_move(robot)
 
-transitions = get_transition_matrix()
-print transitions
-#print transitions[0].index(0.09999999999999999)
+# transitions = get_transition_matrix()
+n_mat = get_nothing_vector()
+print n_mat
+#print transitions[0]
+#print transitions[0].index(0.15)
